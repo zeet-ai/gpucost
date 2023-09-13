8f185429de0e4e8428b31db525981c2e8f5c87ac
Chandler Roth <hey@chandlerroth.com>
2023-09-12 21:11:59 -0500

    Only show provider logos that match provider search

--- src/components/ProvidersCell.tsx
+++ src/components/ProvidersCell.tsx
@@ -3,13 +3,26 @@ import { Flex } from "@chakra-ui/react";
 import { CellContext } from "@tanstack/react-table";
 import { ProviderIcon } from "./ProviderIcon";
 import { GpuRow } from "@/data/gpus";
+import { useSearch } from "@/context/Search";
 
 export const ProvidersCell = ({
   info,
 }: {
   info: CellContext<GpuRow, string | null | undefined>;
 }) => {
+  const { columnFilters } = useSearch();
   const providers = info.getValue()?.split(", ");
+
+  const hasProvidersFilter = (provider: string) => {
+    const providerFilter = columnFilters?.find(
+      (columnFilter) => columnFilter.id === "availableProviders",
+    );
+    if (providerFilter) {
+      const regex = new RegExp(providerFilter?.value as string, "i");
+      return regex.test(provider);
+    }
+    return true;
+  };
 
   if (providers?.length === 0 || providers?.[0] === "") {
     return null;
@@ -17,7 +30,7 @@ export const ProvidersCell = ({
 
   return (
     <Flex gap="4" alignItems="center">
-      {providers?.map((provider) => (
+      {providers?.filter(hasProvidersFilter).map((provider) => (
         <Flex key={provider} alignItems="center" gap="1">
           <ProviderIcon provider={provider as ProviderKind} />
           {provider}
